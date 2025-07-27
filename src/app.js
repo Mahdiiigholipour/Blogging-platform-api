5eb6ecf3b5326a2fc4a355cd7dee34ae07d859e4
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-27 20:01:01 +0330

    add express parsers for parse body content

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const router = require("./blog/router");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require("./config/mongoose");
 
 app.use(router);
