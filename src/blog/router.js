840ee8db1c97ad44aa377ecfef1688655e80103c
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-27 19:52:58 +0330

    add(/blog/:id) : get, update and delete blog by id

--- src/blog/router.js
+++ src/blog/router.js
@@ -2,3 +2,9 @@ const router = require("express").Router();
 const Controller = require("./controller");
 
 router.route("/blog").get(Controller.getAllBlogs).post(Controller.createBlog);
+
+router
+  .route("/blog/:id")
+  .get(Controller.getBlogById)
+  .put(Controller.updateBlogById)
+  .delete(Controller.deleteBlogById);
