5dfb6af31689565756febbf8789a381adeb61a0b
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-27 20:22:43 +0330

    add : create and use not found router and other exceptions handler in express

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = require("./blog/router");
+const {
+  notFoundHandler,
+  exceptionHandler,
+} = require("./middleware/errorHandler");
 
 const app = express();
 
@@ -8,4 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 require("./config/mongoose");
 
 app.use(router);
+
+app.use(notFoundHandler);
+app.use(exceptionHandler);
 module.exports = app;
