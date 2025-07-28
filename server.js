0a37b6455f2befe5bf38775910cb32b402d8553e
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-28 14:15:51 +0330

    refactor server.js and how connect to mongo

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const http = require("http");
 const app = require("./src/app");
 const { PORT } = require("./src/config");
+const connectDB = require("./src/config/mongoose");
 
-const server = http.createServer(app);
+(async () => {
+  try {
+    const server = http.createServer(app);
+    await connectDB();
 
-server.listen(PORT, () =>
-  console.log(`server run on http://localhost:${PORT}`)
-);
+    server.listen(PORT, () =>
+      console.log(`server run on http://localhost:${PORT}`)
+    );
+
+    // بستن اتصال و سرور به‌صورت ایمن
+    const gracefulShutdown = async () => {
+      await mongoose.disconnect();
+      server.close(() => process.exit(0));
+    };
+    process.on("SIGINT", gracefulShutdown);
+    process.on("SIGTERM", gracefulShutdown);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
