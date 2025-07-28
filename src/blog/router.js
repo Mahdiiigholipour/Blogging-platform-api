43bd0f505a99c2c24c4ea4aff49c39b3022b3a92
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-07-28 13:47:52 +0330

    fix(router): about validateReq

--- src/blog/router.js
+++ src/blog/router.js
@@ -1,18 +1,18 @@
 const router = require("express").Router();
-const validate = require("../middleware/validator");
+const validateReq = require("../middleware/validateRequest");
 const Controller = require("./controller");
 const Validation = require("./validation");
 
 router
   .route("/blog")
-  .get(Validation.getAllBlogs, validate, Controller.getAllBlogs)
-  .post(Validation.createBlog, validate, Controller.createBlog);
+  .get(Validation.getAllBlogs, validateReq, Controller.getAllBlogs)
+  .post(Validation.createBlog, validateReq, Controller.createBlog);
 
 router
-  .use(Validation.idInParams, validate)
+  .use(Validation.idInParams, validateReq)
   .route("/blog/:id")
   .get(Controller.getBlogById)
-  .put(Validation.updateBlogById, validate, Controller.updateBlogById)
+  .put(Validation.updateBlogById, validateReq, Controller.updateBlogById)
   .delete(Controller.deleteBlogById);
 
 module.exports = router;
