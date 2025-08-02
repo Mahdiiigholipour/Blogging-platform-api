2ceb0e02c1c0d0ee6e81a493d5aaaf7adb93edd2
Mahdiiigholipour <mahdigholipourprogramming@gmail.com>
2025-08-02 15:27:25 +0330

    fix(auth): register and login responses

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -3,6 +3,10 @@ exports.register = async function (req, res, next) {
   try {
     const { name, email, password } = req.body;
     const result = await AuthService.register({ name, email, password });
+    res.status(201).json({
+      message: "registered new admin successfully!",
+      data: result,
+    });
   } catch (err) {
     next(err);
   }
@@ -12,6 +16,7 @@ exports.login = async function (req, res, next) {
   try {
     const { email, password } = req.body;
     const result = await AuthService.login({ email, password });
+    res.status(200).json({ message: "logged in successfully!", data: result });
   } catch (err) {
     next(err);
   }
